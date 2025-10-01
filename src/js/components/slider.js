436eef15cec64d19a14425b2baac063c4618ab10
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-10-01 22:55:46 +1200

    Блок доп.услуги

---
+++ src/js/components/slider.js
@@ -0,0 +1,43 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+function initServicesSlider() {
+    const servicesSwiper = new Swiper('.services__swiper', {
+        modules: [Navigation, Pagination],
+        slidesPerView: 4,
+        spaceBetween: 20,
+        speed: 800,
+        pagination: {
+            el: '.services__swiper-pagination',
+            type: 'progressbar',
+        },
+        navigation: {
+            nextEl: '.services__swiper-button-next',
+            prevEl: '.services__swiper-button-prev',
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 16,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 16,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            1440: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+        },
+    });
+}
+
+export { initServicesSlider };
+
--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,14 @@
 import { initVideoAnimation } from './components/home.js';
 import { initTabs, initLoadingTabs } from './components/tabs.js';
 import { initAnimations } from './components/animations.js';
+import { initServicesSlider } from './components/slider.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initVideoAnimation();
     initTabs();
     initLoadingTabs();
     initAnimations();
+    initServicesSlider();
 });
 
 
