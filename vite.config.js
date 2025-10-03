e0306bf6dde1c2c91d9a8e2c58cbe76943267ec7
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-10-03 23:47:57 +1200

    билд со страницей о компании

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+        company: resolve(__dirname, 'src/company.html'),
         },
       output: {
         entryFileNames: 'js/[name]-[hash].js',
